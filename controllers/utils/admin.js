e1c897d3ac1ee2b8bf10afceb445961089fa8f08
ELGERSON8098 <20220175@ricaldone.edu.sv>
2024-04-03 10:47:54 -0500

    Hola

--- controllers/utils/admin.js
+++ controllers/utils/admin.js
@@ -65,7 +65,7 @@ const loadTemplate = async () => {
                         <a class="nav-link text-uppercase text-dark" href="../publica/HistorialDeCompras.html">Reservas</a>
                     </li>
                     <li class="nav-item px-2 py-2">
-                        <a class="nav-link text-uppercase text-dark" href="../publica/HistorialDeCompras.html">Administradores</a>
+                        <a class="nav-link text-uppercase text-dark" href="../admin/administrador.html">Administradores</a>
                     </li>
                 </ul>
             </div>
