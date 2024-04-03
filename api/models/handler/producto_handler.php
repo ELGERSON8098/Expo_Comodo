7c14291c71b7f9999e1e783b71989294d61a8588
TeresaRuiz <107508345+TeresaRuiz@users.noreply.github.com>
2024-04-03 12:15:57 -0600

    Update producto_handler.php

--- api/models/handler/producto_handler.php
+++ api/models/handler/producto_handler.php
@@ -2,85 +2,55 @@
 // Se incluye la clase para trabajar con la base de datos.
 require_once('../../helpers/database.php');
 /*
-*	Clase para manejar el comportamiento de los datos de la tabla PRODUCTO.
-*/
-class ProductoHandler
+ *  Clase para manejar el comportamiento de los datos de la tabla administrador.
+ */
+class productoHandler
 {
     /*
-    *   Declaración de atributos para el manejo de datos.
-    */
-    protected $id = null;
-    protected $nombre = null;
+     *  Declaración de atributos para el manejo de datos.
+     */
+
+    protected $id_producto = null;
+    protected $nombre_producto = null;
     protected $descripcion = null;
-    protected $codigo = null;
-    protected $referencia = null;
+    protected $codigo_interno = null;
+    protected $referencia_proveedor = null;
     protected $imagen = null;
-    // Constante para establecer la ruta de las imágenes.
-    const RUTA_IMAGEN = '../../images/productos/';
+    protected $id_subcategoria = null;
+    protected $id_administrador = null;
 
-    /*
-    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
-    */
-    public function searchRows()
-    {
-        $value = '%' . Validator::getSearchValue() . '%';
-        $sql = 'SELECT id_producto, nombre_producto, descripcion, codigo_interno, Referencia_provedor, imagen
-                FROM tbproductos';
-        $params = array($value, $value);
-        return Database::getRows($sql, $params);
-    }
-
-    public function createRow()
-    {
-        $sql = 'INSERT INTO tbproductos(id_producto, nombre_producto, descripcion, codigo_interno, Referencia_provedor, imagen)
-                VALUES(?, ?, ?, ?, ?, ?)';
-        $params = array($this->id, $this->nombre, $this->descripcion, $this->codigo, $this->referencia, $this->imagen);
-        return Database::executeRow($sql, $params);
-    }
-
+    
     public function readAll()
     {
-        $sql = 'SELECT id_producto, nombre_producto, descripcion, codigo_interno, Referencia_provedor, imagen
-                FROM tbproductos';
-                
+        $sql = 'SELECT id_producto, nombre_producto, descripcion, codigo_interno, Referencia_provedor, imagen, id_subcategoria, id_administrador
+        FROM tbproductos';
         return Database::getRows($sql);
     }
 
     public function readOne()
     {
-        $sql = 'SELECT id_producto, nombre_producto, descripcion, codigo_interno, Referencia_provedor, imagen
-                FROM tbproductos
-                WHERE id_producto = ?';
-        $params = array($this->id);
-        return Database::getRow($sql, $params);
-    }
-
-    public function readFilename()
-    {
-        $sql = 'SELECT imagen
-                FROM tbproductos
-                WHERE id_producto = ?';
-        $params = array($this->id);
+        $sql = 'SELECT id_producto, nombre_producto, descripcion, codigo_interno, Referencia_provedor, imagen, id_subcategoria, id_administrador
+        FROM tbproductos
+        WHERE id_producto = ?';
+        $params = array($this->id_producto);
         return Database::getRow($sql, $params);
     }
 
     public function updateRow()
     {
         $sql = 'UPDATE tbproductos
-                SET imagen = ?, nombre_producto = ?, descripcion = ?, codigo_interno = ?, Referencia_provedor = ?, imagen = ?
+                SET nombre_producto = ?, descripcion = ?, codigo_interno = ?, Referencia_provedor = ?, imagen = ?, id_subcategoria = ?, id_administrador = ?
                 WHERE id_producto = ?';
-        $params = array($this->imagen, $this->nombre, $this->descripcion, $this->codigo, $this->referencia, $this->id);
+        $params = array($this->nombre_producto, $this->descripcion, $this->codigo_interno, $this->referencia_proveedor, $this->imagen, $this->id_subcategoria, $this->id_administrador, $this->id_producto);
         return Database::executeRow($sql, $params);
     }
 
     public function deleteRow()
     {
         $sql = 'DELETE FROM tbproductos
-                WHERE id_producto = ?';
-        $params = array($this->id);
+        WHERE id_producto = ?';
+        $params = array($this->id_producto);
         return Database::executeRow($sql, $params);
     }
-
- 
-
 }
+
