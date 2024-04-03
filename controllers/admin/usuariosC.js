bdee49d8569156009b8bdfa1c028919ec053fd78
ELGERSON8098 <20220175@ricaldone.edu.sv>
2024-04-03 10:41:40 -0500

    CambioGeneral

--- controllers/admin/usuariosC.js
+++ controllers/admin/usuariosC.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
     // Se establece el título del contenido principal.
-    MAIN_TITLE.textContent = 'Gestionar Trabajadores';
+    MAIN_TITLE.textContent = 'Gestionar Clientes';
     // Llamada a la función para llenar la tabla con los registros existentes.
     fillTable();
 });
