9e2df4333ac4efce04828c27e1c1ea93d33a36b2
dhakhax28 <20220096@ricaldone.edu.sv>
2024-04-08 09:17:07 -0600

    cambios dk (nombre de trabajadores y direcciones)

--- controllers/admin/trabajador.js
+++ controllers/admin/trabajador.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Llamada a la función para mostrar el encabezado y pie del documento.
     loadTemplate();
     // Se establece el título del contenido principal.
-    MAIN_TITLE.textContent = 'Gestionar administradores';
+    MAIN_TITLE.textContent = 'Gestionar empleados';
     // Llamada a la función para llenar la tabla con los registros existentes.
     fillTable();
 });
