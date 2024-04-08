3f72ab29736235fff42bd3c462422e32cd514128
TeresaRuiz <107508345+TeresaRuiz@users.noreply.github.com>
2024-04-08 10:50:08 -0600

    Corrección de trigger y función

--- Expo_Comodo$.sql
+++ Expo_Comodo$.sql
@@ -141,34 +141,41 @@ CREATE TABLE tb_detalles_reservas (
   CONSTRAINT fk_detalle_producto FOREIGN KEY (id_detalle_producto) REFERENCES tb_detalles_productos(id_detalle_producto)
 );
 
-CREATE FUNCTION ObtenerIdNivelAdministrador()
+-- Crear la función para obtener el ID de nivel de administrador
+DELIMITER //
+
+CREATE FUNCTION obtener_id_nivel_administrador()
 RETURNS INT
 BEGIN
     DECLARE nivel_id INT;
 
-    -- Obtener el ID del nivel de usuario correspondiente a "Administrador"
-    SELECT id_nivel_usuario INTO nivel_id FROM tbniveles_usuario WHERE nombre_nivel = 'Administrador';
+    -- Obtener el ID del nivel de usuario correspondiente a "administrador"
+    SELECT id_nivel_usuario INTO nivel_id FROM tb_niveles_usuarios WHERE nombre_nivel = 'administrador';
 
     RETURN nivel_id;
 END//
 
 DELIMITER ;
 
+SELECT obtener_id_nivel_administrador()
+
+-- Crear el trigger antes de insertar en tb_niveles_usuario
 DELIMITER //
 
 CREATE TRIGGER before_insert_nivel_usuario
-BEFORE INSERT ON tbniveles_usuario
+BEFORE INSERT ON tb_niveles_usuarios
 FOR EACH ROW
 BEGIN
     -- Verificar si la tabla está vacía
     DECLARE count_rows INT;
-    SELECT COUNT(*) INTO count_rows FROM tbniveles_usuario;
+    SELECT COUNT(*) INTO count_rows FROM tb_niveles_usuarios;
     
-    -- Si la tabla está vacía, forzar el nombre del nivel a "Administrador"
+    -- Si la tabla está vacía, forzar el nombre del nivel a "administrador"
     IF count_rows = 0 THEN
-        SET NEW.nombre_nivel = 'Administrador';
+        SET NEW.nombre_nivel = 'administrador';
     END IF;
 END;
 //
 
 DELIMITER ;
+
