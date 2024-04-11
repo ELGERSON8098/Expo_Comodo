fb27f3b5f89638826a1f0b9f6244ee1c385a277b
ELGERSON8098 <129894493+ELGERSON8098@users.noreply.github.com>
2024-04-11 13:58:35 -0300

    Update funciones_comodos.sql

--- Base_de_datos_expo/funciones_comodos.sql
+++ Base_de_datos_expo/funciones_comodos.sql
@@ -1,39 +1,18 @@
 USE expo_comodos;
 
--- Crear la función para obtener el ID de nivel de administrador
+
 DELIMITER //
 
-CREATE FUNCTION obtener_id_nivel_administrador()
-RETURNS INT
+CREATE FUNCTION generar_saludo(nombre_usuario VARCHAR(100))
+RETURNS VARCHAR(255)
 BEGIN
-    DECLARE nivel_id INT;
-
-    -- Obtener el ID del nivel de usuario correspondiente a "administrador"
-    SELECT id_nivel_usuario INTO nivel_id FROM tb_niveles_usuarios WHERE nombre_nivel = 'administrador';
-
-    RETURN nivel_id;
-END//
-
-DELIMITER ;
-
-SELECT obtener_id_nivel_administrador()
-
--- Crear el trigger antes de insertar en tb_niveles_usuario
-DELIMITER //
-
-CREATE TRIGGER before_insert_nivel_usuario
-BEFORE INSERT ON tb_niveles_usuarios
-FOR EACH ROW
-BEGIN
-    -- Verificar si la tabla está vacía
-    DECLARE count_rows INT;
-    SELECT COUNT(*) INTO count_rows FROM tb_niveles_usuarios;
-    
-    -- Si la tabla está vacía, forzar el nombre del nivel a "administrador"
-    IF count_rows = 0 THEN
-        SET NEW.nombre_nivel = 'administrador';
-    END IF;
+    DECLARE saludo VARCHAR(255);
+    SET saludo = CONCAT('¡Hola ', nombre_usuario, '! Bienvenido/a.');
+    RETURN saludo;
 END;
 //
 
 DELIMITER ;
+
+SELECT nombre, generar_saludo(nombre) AS saludo FROM tb_usuarios;
+
