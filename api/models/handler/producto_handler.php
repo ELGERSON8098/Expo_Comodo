be17090991a46e278d075fa4c3086a07bf313426
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-06-20 12:34:27 -0600

    actualizar productos 100%

--- api/models/handler/producto_handler.php
+++ api/models/handler/producto_handler.php
@@ -123,7 +123,7 @@ class ProductoHandler
         $sql = 'UPDATE tb_productos
             SET nombre_producto = ?, codigo_interno = ?, referencia_proveedor = ?, precio = ?, id_marca = ?, id_genero = ?, id_categoria = ?, id_material = ?, id_descuento = ?, imagen = ?
             WHERE id_producto = ?';
-        $params = array($this->nombre_producto, $this->codigo_interno, $this->referencia_proveedor, $this->precio, $this->id_marca, $this->id_genero, $this->id_categoria, $this->id_material, $this->id_descuento, $this->imagen, $this->id_producto);
+        $params = array($this->nombre_producto, $this->codigo_interno, $this->referencia_proveedor, $this->precio, $this->id_marca, $this->id_genero, $this->id_categoria, $this->id_material, $this->id_descuento, $this->imagen_producto, $this->id_producto);
         return Database::executeRow($sql, $params);
     }
 
