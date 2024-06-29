aec6f3f4abd35bca85d73da55c36afd8c91fb451
ELGERSON8098 <129894493+ELGERSON8098@users.noreply.github.com>
2024-06-28 23:03:45 -0300

    Update descuento.php

--- api/services/admin/descuento.php
+++ api/services/admin/descuento.php
@@ -56,22 +56,22 @@ if (isset($_GET['action'])) {
                     $result['error'] = 'Descuento inexistente';
                 }
                 break;
-            case 'updateRow':
-                $_POST = Validator::validateForm($_POST);
-                if (
-                    !$descuento->setid($_POST['idDescuento']) or
-                    !$descuento->setNombre($_POST['nombreDescuento']) or
-                    !$descuento->setDesc($_POST['nombreDesc']) or
-                    !$descuento->setvalor($_POST['ValorM'])
-                ) {
-                    $result['error'] = $descuento->getDataError();
-                } elseif ($descuento->updateRow()) {
-                    $result['status'] = 1;
-                    $result['message'] = 'Descuento modificado correctamente';
-                } else {
-                    $result['error'] = 'Ocurrió un problema al modificar el descuento';
-                }
-                break;
+                case 'updateRow':
+                    $_POST = Validator::validateForm($_POST);
+                    if (
+                        !$descuento->setid($_POST['idDescuento']) or
+                        !$descuento->setNombre($_POST['nombreDescuento']) or
+                        !$descuento->setDesc($_POST['nombreDesc']) or
+                        !$descuento->setvalor($_POST['ValorM'])
+                    ) {
+                        $result['error'] = $descuento->getDataError();
+                    } elseif ($descuento->updateRow()) {
+                        $result['status'] = 1;
+                        $result['message'] = 'Descuento modificado correctamente';
+                    } else {
+                        $result['error'] = 'Ocurrió un problema al modificar el descuento';
+                    }
+                    break;                
             case 'deleteRow':
                 if (
                     !$descuento->setid($_POST['idDescuento'])
