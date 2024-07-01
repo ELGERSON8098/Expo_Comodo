56ac1bb2f9de90a999e3e23e0e9cfc4169d722cb
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-07-01 08:08:44 -0600

    ortografía

--- api/models/data/administrador_data.php
+++ api/models/data/administrador_data.php
@@ -67,13 +67,13 @@ class AdministradorData extends AdministradorHandler
 
     public function setAlias($value, $min = 6, $max = 25) {
         if (!Validator::validateAlphanumeric($value)) {
-            $this->data_error = 'El alias debe ser un valor alfanumérico';
+            $this->data_error = 'El usuario debe ser un valor alfanumérico';
             return false;
         } elseif (Validator::validateLength($value, $min, $max)) {
             $this->alias = $value;
             return true;
         } else {
-            $this->data_error = 'El alias debe tener una longitud entre ' . $min . ' y ' . $max;
+            $this->data_error = 'El usuario debe tener una longitud entre ' . $min . ' y ' . $max;
             return false;
         }
     }
