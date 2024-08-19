0a7e0f1775f29f01df5841d3443f16db64e046b1
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-08-19 15:34:01 -0600

    Agregando metodo para grafica en marcas

--- api/models/handler/marca_handler.php
+++ api/models/handler/marca_handler.php
@@ -70,4 +70,52 @@ class marcaHandler
         return Database::executeRow($sql, $params);
     }
 
+    public function ventasPorCategoriaFecha($fechaInicio, $fechaFin) {
+        $sql = 'SELECT 
+    c.nombre_categoria, 
+    SUM(dr.cantidad * dr.precio_unitario) AS total_ventas
+    FROM 
+    tb_detalles_reservas dr
+    INNER JOIN 
+    tb_reservas r ON dr.id_reserva = r.id_reserva
+    INNER JOIN 
+    tb_detalles_productos dp ON dr.id_detalle_producto = dp.id_detalle_producto
+    INNER JOIN 
+    tb_productos p ON dp.id_producto = p.id_producto
+    INNER JOIN 
+    tb_categorias c ON p.id_categoria = c.id_categoria
+    WHERE 
+    r.fecha_reserva BETWEEN ? AND ?
+    GROUP BY 
+    c.nombre_categoria
+    LIMIT 5;';
+        $params = array($fechaInicio, $fechaFin);
+        return Database::getRows($sql, $params);
+    }
+
+    public function ventasPorMarcasFecha($fechaInicio, $fechaFin) {
+        $sql = 'SELECT 
+    m.marca AS nombre_marca, 
+    r.fecha_reserva, 
+    SUM(dr.cantidad * dr.precio_unitario) AS total_ventas
+    FROM 
+    tb_detalles_reservas dr
+    INNER JOIN 
+    tb_reservas r ON dr.id_reserva = r.id_reserva
+    INNER JOIN 
+    tb_detalles_productos dp ON dr.id_detalle_producto = dp.id_detalle_producto
+    INNER JOIN 
+    tb_productos p ON dp.id_producto = p.id_producto
+    INNER JOIN 
+    tb_marcas m ON p.id_marca = m.id_marca
+    WHERE 
+    r.fecha_reserva BETWEEN ? AND ?
+    GROUP BY 
+    m.marca, r.fecha_reserva
+    ORDER BY 
+    r.fecha_reserva ASC;';
+        $params = array($fechaInicio, $fechaFin);
+        return Database::getRows($sql, $params);
+    }
+     
 }
