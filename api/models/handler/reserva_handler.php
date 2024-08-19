d0d22f41332a0891dacca8a3766f4820bfef7600
TeresaRuiz <20220651@ricaldone.edu.sv>
2024-08-19 09:42:22 -0600

    Añadiendo metodos para la segunda grafica personalizada

--- api/models/handler/reserva_handler.php
+++ api/models/handler/reserva_handler.php
@@ -305,4 +305,30 @@ class ReservaHandler
         $params = array($fechaInicio, $fechaFin);
         return Database::getRows($sql, $params);
     }
+
+    public function ventasPorMarcasFecha($fechaInicio, $fechaFin) {
+        $sql = 'SELECT 
+    m.marca AS nombre_marca, 
+    r.fecha_reserva, 
+    SUM(dr.cantidad * dr.precio_unitario) AS total_ventas
+    FROM 
+    tb_detalles_reservas dr
+    INNER JOIN 
+    tb_reservas r ON dr.id_reserva = r.id_reserva
+    INNER JOIN 
+    tb_detalles_productos dp ON dr.id_detalle_producto = dp.id_detalle_producto
+    INNER JOIN 
+    tb_productos p ON dp.id_producto = p.id_producto
+    INNER JOIN 
+    tb_marcas m ON p.id_marca = m.id_marca
+    WHERE 
+    r.fecha_reserva BETWEEN ? AND ?
+    GROUP BY 
+    m.marca, r.fecha_reserva
+    ORDER BY 
+    r.fecha_reserva ASC;';
+        $params = array($fechaInicio, $fechaFin);
+        return Database::getRows($sql, $params);
+    }
+     
 }
