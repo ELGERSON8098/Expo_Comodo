dcdb7ab68d1b36eefcfd8fd4eddf69e76057acb3
ELGERSON8098 <20220175@ricaldone.edu.sv>
2024-09-14 13:13:27 -0600

    cambios

--- api/models/handler/categoria_handler.php
+++ api/models/handler/categoria_handler.php
@@ -18,7 +18,7 @@ class CategoriaHandler
     protected $imagen = null;
 
     // Constante para establecer la ruta de las imágenes.
-    const RUTA_IMAGEN = '../Expo_Comodo/api/images';
+    const RUTA_IMAGEN = '../../images/categorias/';
 
     /*
      *  Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
