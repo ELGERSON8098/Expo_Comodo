e4cc2c4f4128cd3893d65fee0864929a558dbdac
ELGERSON8098 <20220175@ricaldone.edu.sv>
2024-10-15 22:01:02 -0600

    cambio

--- api/reports/admin/usuarios_Registrados.php
+++ api/reports/admin/usuarios_Registrados.php
@@ -15,7 +15,7 @@ $pdf->Ln(10); // Primer salto de línea
 $dataUsuario = $usuario->usuariosRegistrados();
 
 if ($dataUsuario) {
-    $pdf->SetFillColor(27, 88, 169);
+    $pdf->SetFillColor(38, 15, 189);
     $pdf->SetFont('Times', 'B', 11);
     $pdf->Cell(50, 10, 'Nombre usuario', 1, 0, 'C', 1);
     $pdf->Cell(60, 10, $pdf->encodeString('Correo'), 1, 0, 'C', 1);
