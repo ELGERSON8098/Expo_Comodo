43fe808c3d9e5e6b346087559b4fe6f983dc554f
ELGERSON8098 <20220175@ricaldone.edu.sv>
2024-10-16 22:49:49 -0600

    cambios

--- api/models/handler/marca_handler.php
+++ api/models/handler/marca_handler.php
@@ -53,135 +53,135 @@ class marcaHandler
         // SQL Query
         $sql = '
         WITH VentasMensuales AS (
-            SELECT 
-                m.marca AS NombreMarca,
-                p.nombre_producto AS NombreProducto,
-                SUM(dr.cantidad) AS CantidadReservada,
-                DATE_FORMAT(r.fecha_reserva, "%Y-%m") AS Mes,
-                SUM(
-                    dr.cantidad * dr.precio_unitario * 
-                    (1 - IFNULL(d.valor, 0) / 100)
-                ) AS TotalVentasMarca,
-                FORMAT(
-                    (SUM(
-                        dr.cantidad * dr.precio_unitario * 
-                        (1 - IFNULL(d.valor, 0) / 100)
-                    ) * 100) / (
-                        SELECT SUM(
-                            dr2.cantidad * dr2.precio_unitario * 
-                            (1 - IFNULL(d2.valor, 0) / 100)
-                        )
-                        FROM tb_detalles_reservas dr2
-                        INNER JOIN tb_reservas r2 ON dr2.id_reserva = r2.id_reserva
-                        LEFT JOIN tb_productos p2 ON dr2.id_detalle_producto = p2.id_producto
-                        LEFT JOIN tb_descuentos d2 ON p2.id_descuento = d2.id_descuento
-                        WHERE r2.estado_reserva = "Aceptado"
-                        AND DATE_FORMAT(r2.fecha_reserva, "%Y-%m") = DATE_FORMAT(r.fecha_reserva, "%Y-%m")
-                    ), 2
-                ) AS PorcentajeVentasMarca
-            FROM 
-                tb_marcas m
-            INNER JOIN 
-                tb_productos p ON m.id_marca = p.id_marca
-            INNER JOIN 
-                tb_detalles_productos dp ON p.id_producto = dp.id_producto
-            INNER JOIN 
-                tb_detalles_reservas dr ON dp.id_detalle_producto = dr.id_detalle_producto
-            INNER JOIN 
-                tb_reservas r ON dr.id_reserva = r.id_reserva
-            LEFT JOIN 
-                tb_descuentos d ON p.id_descuento = d.id_descuento
-            WHERE 
-                r.estado_reserva = "Aceptado"
-            GROUP BY 
-                m.marca, p.nombre_producto, DATE_FORMAT(r.fecha_reserva, "%Y-%m")
-        ),
-        
-        VentasAnteriores AS (
-            SELECT 
-                NombreMarca,
-                NombreProducto,
-                AVG(TotalVentasMarca) AS PromedioMensual
-            FROM 
-                VentasMensuales
-            GROUP BY 
-                NombreMarca, NombreProducto
-        ),
-        
-        VentasActuales AS (
-            SELECT 
-                v1.NombreMarca,
-                v1.NombreProducto,
-                v1.Mes AS MesActual,
-                v1.CantidadReservada,
-                v1.TotalVentasMarca,
-                v1.PorcentajeVentasMarca,
-                DATE_FORMAT(DATE_ADD(STR_TO_DATE(v1.Mes, "%Y-%m-01"), INTERVAL 1 MONTH), "%Y-%m") AS MesSiguiente
-            FROM 
-                VentasMensuales v1
-        ),
-        
-        VentasPronosticadas AS (
-            SELECT
-                va.NombreMarca,
-                va.NombreProducto,
-                va.MesActual,
-                va.CantidadReservada,
-                va.TotalVentasMarca,
-                va.PorcentajeVentasMarca,
-                COALESCE(va_prev.PromedioMensual, 0) AS PromedioMensual,
-                COALESCE(
-                    (va.PorcentajeVentasMarca / 100) * va_prev.PromedioMensual,
-                    0
-                ) AS PrediccionVentasSiguienteMes
-            FROM 
-                VentasActuales va
-            LEFT JOIN 
-                VentasAnteriores va_prev 
-                ON va.NombreMarca = va_prev.NombreMarca
-                AND va.NombreProducto = va_prev.NombreProducto
-        )
-        
-        SELECT 
-            vp.NombreMarca,
-            vp.NombreProducto,
-            vp.MesActual,
-            vp.CantidadReservada,
-            vp.TotalVentasMarca,
-            vp.PorcentajeVentasMarca,
-            FORMAT(vp.PrediccionVentasSiguienteMes, 2) AS PrediccionVentasSiguienteMes,
+    SELECT 
+        m.marca AS NombreMarca,
+        p.nombre_producto AS NombreProducto,
+        SUM(dr.cantidad) AS CantidadReservada,
+        DATE_FORMAT(r.fecha_reserva, "%Y-%m") AS Mes,
+        SUM(
+            dr.cantidad * dr.precio_unitario * 
+            (1 - IFNULL(d.valor, 0) / 100)
+        ) AS TotalVentasMarca,
+        FORMAT(
+            (SUM(
+                dr.cantidad * dr.precio_unitario * 
+                (1 - IFNULL(d.valor, 0) / 100)
+            ) * 100) / (
+                SELECT SUM(
+                    dr2.cantidad * dr2.precio_unitario * 
+                    (1 - IFNULL(d2.valor, 0) / 100)
+                )
+                FROM tb_detalles_reservas dr2
+                INNER JOIN tb_reservas r2 ON dr2.id_reserva = r2.id_reserva
+                LEFT JOIN tb_productos p2 ON dr2.id_detalle_producto = p2.id_producto
+                LEFT JOIN tb_descuentos d2 ON p2.id_descuento = d2.id_descuento
+                WHERE r2.estado_reserva = "Aceptado"
+                AND DATE_FORMAT(r2.fecha_reserva, "%Y-%m") = DATE_FORMAT(r.fecha_reserva, "%Y-%m")
+            ), 2
+        ) AS PorcentajeVentasMarca
+    FROM 
+        tb_marcas m
+    INNER JOIN 
+        tb_productos p ON m.id_marca = p.id_marca
+    INNER JOIN 
+        tb_detalles_productos dp ON p.id_producto = dp.id_producto
+    INNER JOIN 
+        tb_detalles_reservas dr ON dp.id_detalle_producto = dr.id_detalle_producto
+    INNER JOIN 
+        tb_reservas r ON dr.id_reserva = r.id_reserva
+    LEFT JOIN 
+        tb_descuentos d ON p.id_descuento = d.id_descuento
+    WHERE 
+        r.estado_reserva = "Aceptado"
+    GROUP BY 
+        NombreMarca, NombreProducto, Mes
+),
+
+VentasAnteriores AS (
+    SELECT 
+        NombreMarca,
+        NombreProducto,
+        AVG(TotalVentasMarca) AS PromedioMensual
+    FROM 
+        VentasMensuales
+    GROUP BY 
+        NombreMarca, NombreProducto
+),
+
+VentasActuales AS (
+    SELECT 
+        v1.NombreMarca,
+        v1.NombreProducto,
+        v1.Mes AS MesActual,
+        v1.CantidadReservada,
+        v1.TotalVentasMarca,
+        v1.PorcentajeVentasMarca,
+        DATE_FORMAT(DATE_ADD(STR_TO_DATE(v1.Mes, "%Y-%m-01"), INTERVAL 1 MONTH), "%Y-%m") AS MesSiguiente
+    FROM 
+        VentasMensuales v1
+),
+
+VentasPronosticadas AS (
+    SELECT
+        va.NombreMarca,
+        va.NombreProducto,
+        va.MesActual,
+        va.CantidadReservada,
+        va.TotalVentasMarca,
+        va.PorcentajeVentasMarca,
+        COALESCE(va_prev.PromedioMensual, 0) AS PromedioMensual,
+        COALESCE(
+            (va.PorcentajeVentasMarca / 100) * va_prev.PromedioMensual,
+            0
+        ) AS PrediccionVentasSiguienteMes
+    FROM 
+        VentasActuales va
+    LEFT JOIN 
+        VentasAnteriores va_prev 
+        ON va.NombreMarca = va_prev.NombreMarca
+        AND va.NombreProducto = va_prev.NombreProducto
+)
+
+SELECT 
+    vp.NombreMarca,
+    vp.NombreProducto,
+    vp.MesActual,
+    vp.CantidadReservada,
+    vp.TotalVentasMarca,
+    vp.PorcentajeVentasMarca,
+    FORMAT(vp.PrediccionVentasSiguienteMes, 2) AS PrediccionVentasSiguienteMes,
+    CASE 
+        WHEN vp.TotalVentasMarca = 0 THEN NULL
+        ELSE
             CASE 
-                WHEN vp.TotalVentasMarca = 0 THEN NULL
+                WHEN vp.PrediccionVentasSiguienteMes > vp.TotalVentasMarca THEN
+                    CONCAT(
+                        "Aumento estimado de ", 
+                        FORMAT(
+                            LEAST(
+                                (vp.PrediccionVentasSiguienteMes - vp.TotalVentasMarca) / vp.TotalVentasMarca * 100, 
+                                100
+                            ), 2
+                        ), 
+                        "%"
+                    )
                 ELSE
-                    CASE 
-                        WHEN vp.PrediccionVentasSiguienteMes > vp.TotalVentasMarca THEN
-                            CONCAT(
-                                "Aumento estimado de ", 
-                                FORMAT(
-                                    LEAST(
-                                        (vp.PrediccionVentasSiguienteMes - vp.TotalVentasMarca) / vp.TotalVentasMarca * 100, 
-                                        100
-                                    ), 2
-                                ), 
-                                "%"
-                            )
-                        ELSE
-                            CONCAT(
-                                "Reducción estimada de ", 
-                                FORMAT(
-                                    LEAST(
-                                        (vp.TotalVentasMarca - vp.PrediccionVentasSiguienteMes) / vp.TotalVentasMarca * 100, 
-                                        100
-                                    ), 2
-                                ), 
-                                "%"
-                            )
-                    END
-            END AS PorcentajeYMensaje
-        FROM 
-            VentasPronosticadas vp
-        ORDER BY 
-            vp.MesActual ASC, vp.NombreMarca ASC, vp.NombreProducto ASC';
+                    CONCAT(
+                        "Reducción estimada de ", 
+                        FORMAT(
+                            LEAST(
+                                (vp.TotalVentasMarca - vp.PrediccionVentasSiguienteMes) / vp.TotalVentasMarca * 100, 
+                                100
+                            ), 2
+                        ), 
+                        "%"
+                    )
+            END
+    END AS PorcentajeYMensaje
+FROM 
+    VentasPronosticadas vp
+ORDER BY 
+    vp.MesActual ASC, vp.NombreMarca ASC, vp.NombreProducto ASC';
         
         // Execute the query
         return Database::getRows($sql);
